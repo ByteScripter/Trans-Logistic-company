90fe873dfe4671a9b0ad5ec120a8bc6f896d0517
MR.byte <mr.byte101@gmail.com>
2023-04-02 23:01:41 +0200

    Our Clients started

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,8 +1,21 @@
 $(function () {
+	// выбираем необходимые элементы DOM
 	var $burger = $('.header-burger'),
-		$nav = $('.header-nav__list');
+		$nav = $('.header-nav__list'),
+		$navLinks = $('.header-nav__link');
 
+	// функция для закрытия меню
+	function closeNav() {
+		// удаляем классы из body и элементов меню
+		$('body').removeClass('lock');
+		$nav.removeClass('active-burger');
+		$burger.removeClass('active-burger--rotate');
+		$('.header-burger__burger-menu').removeClass('active-burger--rotate');
+	}
+
+	// обработчик клика по кнопке бургера
 	$burger.on('click', function (event) {
+		// добавляем/удаляем классы у body и элементов меню
 		$('body').toggleClass('lock');
 		$nav.toggleClass('active-burger');
 		$burger.toggleClass('active-burger--rotate');
@@ -10,13 +23,21 @@ $(function () {
 		event.stopPropagation();
 	});
 
+	// обработчик клика по документу
 	$(document).on('click', function (event) {
+		// если клик был вне меню и кнопки бургера
 		if (!$nav.is(event.target) && !$burger.is(event.target) && !$nav.has(event.target).length && !$burger.has(event.target).length) {
-			$('body').add($nav, $burger, $('.header-burger__burger-menu')).removeClass('lock active-burger active-burger--rotate');
+			// закрываем меню
+			closeNav();
 		}
 	});
-});
 
+	// обработчик клика по ссылкам меню
+	$navLinks.on('click', function () {
+		// закрываем меню
+		closeNav();
+	});
+});
 
 
 
